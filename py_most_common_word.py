382ce332fcca9b1ff2138be3b387b8d596ead7e7
llewyn.jh <llewyn.jh@gmail.com>
2021-05-05 03:55:55 +0900

    add remarks

--- py_most_common_word.py
+++ py_most_common_word.py
@@ -6,10 +6,12 @@ import collections
 def most_common_word(paragraph: str, banned: list) -> str:
     """Return most common word except for banned words"""
 
+    # Data processing
     words = [word for word in re.sub(r"[^\w]", " ", paragraph)
     .lower()
     .split() if word not in banned]
 
+    # Reorder a dictionay by values
     counts = collections.Counter(words)
     return sorted(counts.items(), key=lambda x : x[1], reverse=True)[0][0]
 
