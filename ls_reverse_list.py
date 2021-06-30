3a43540af70d7383945d5fcc9bec07b1aef0e034
llewyn.jh <llewyn.jh@gmail.com>
2021-07-01 00:23:59 +0900

    solve leetcode #2

---
+++ ls_reverse_list.py
@@ -0,0 +1,20 @@
+"""Reverse linked list, leetcode 2"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverse_list(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        node, prev = head, None
+
+        while node:
+            next, node.next = node.next, prev
+            prev, node = node, next
+
+        return prev
