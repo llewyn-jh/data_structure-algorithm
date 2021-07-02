dda0d78dbe1a61cddddf74b63769ebe35904205a
llewyn.jh <llewyn.jh@gmail.com>
2021-07-03 02:01:45 +0900

    add another solution

--- ls_add_two_numbers.py
+++ ls_add_two_numbers.py
@@ -1,6 +1,6 @@
 """Add two numbers, leetcode 2"""
 class Solution(object):
-
+    """Method 1: Convert data type"""
     def reverse_num(self, node):
         """
         node: ListNode
@@ -36,3 +36,37 @@ class Solution(object):
             self.reverse_num(linked_list_2))
 
         return self.to_linked_list(str_num)
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    """Method 2: Full adder"""
+    def add_two_numbers(self, linked_list_1, linked_list_2):
+        """
+        :type linked_list_1: ListNode
+        :type linked_list_2: ListNode
+        :rtype: ListNode
+        """
+
+        head = root = ListNode(0)
+        carry = 0
+
+        while linked_list_1 or linked_list_2 or carry:
+            SUM = 0
+
+            if linked_list_1:
+                SUM += linked_list_1.val
+                linked_list_1 = linked_list_1.next
+
+            if linked_list_2:
+                SUM += linked_list_2.val
+                linked_list_2 = linked_list_2.next
+
+            carry, val = divmod(SUM + carry, 10)
+            head.next = ListNode(val)
+            head = head.next
+
+        return root.next
