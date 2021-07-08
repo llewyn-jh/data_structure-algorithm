0b61043525b09a22488c787c588dab6bd7fb4c2c
llewyn.jh <llewyn.jh@gmail.com>
2021-07-09 02:38:13 +0900

    solve the problem with recursive function

--- ls_swap_nodes_in_pairs.py
+++ ls_swap_nodes_in_pairs.py
@@ -6,7 +6,7 @@
 #         self.val = val
 #         self.next = next
 
-# Method 1
+# Method 1: swap only values
 # 현업에서는 복잡한 구조로 구성되어 단순히 자료의 값만 바꾸는 방법은 실용성이 떨어진다.
 class Solution(object):
     def swap_pairs(self, head):
@@ -24,7 +24,7 @@ class Solution(object):
 
         return head
 
-# Method 2
+# Method 2: swap with iterative structure
 class Solution(object):
     def swap_pairs(self, head):
         """
@@ -32,7 +32,7 @@ class Solution(object):
         :rtype: ListNode
         """
 
-        node = prev =ListNode(None)
+        node = prev = ListNode(None)
         prev.next = head
 
         while head and head.next:
@@ -49,3 +49,19 @@ class Solution(object):
             prev = prev.next.next
 
         return node.next
+
+# Method 3: swap with recursive function
+class Solution(object):
+    def swap_pairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+
+        if head and head.next:
+            prev = head.next
+            head.next = self.swap_pairs(prev.next)
+            prev.next = head
+            return prev
+
+        return head
